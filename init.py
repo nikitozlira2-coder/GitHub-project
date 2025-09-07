f2a143172c66af89fb8259c25afb106ab7744e27
nikitozlira2-coder <nikitozlira2@gmail.com>
2025-09-07 17:33:37 +0300

    init py

---
+++ init.py
@@ -0,0 +1,14 @@
+person1 = {"name": "Alice", "age": 25, "city": "New York"}
+person2 = {"name": "Alice", "age": 10,}
+
+
+if len(person1) > 0:
+    print("Словник не порожній")
+else:
+    print("Словник порожній")
+if len(person2) > 0:
+    print("Словник не порожній")
+else:
+    print("Словник порожній")
+
+print("Hello repo")
